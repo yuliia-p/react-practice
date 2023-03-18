971e9563fea04ef1cc5bf0fca4f2ab915474c6d0
Yuliia Pchelintseva <junoromanova@gmail.com>
2023-03-17 17:52:23 -0700

    bootstrap removed

--- vite-project/src/components/Footer.jsx
+++ vite-project/src/components/Footer.jsx
@@ -1,24 +1,11 @@
 import React from "react";
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+
 
 function Footer() {
     return (
-        <Container>
-          <Row>
-            <Col>
-            image:
-            </Col>
-          </Row>
-          <Row>
-            <Col>1 of 3</Col>
-            <Col>2 of 3</Col>
-            <Col>3 of 3</Col>
-            <Col>4 of 3</Col>
-            <Col>5 of 3</Col>
-            </Row>
-        </Container>
+        <div className="conteiner">
+            <h1>hello footer</h1>
+        </div>
       );
 }
 export default Footer
--- vite-project/src/components/Navbar.jsx
+++ vite-project/src/components/Navbar.jsx
@@ -1,20 +1,33 @@
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
 
 function NavbarComponent() {
     return (
-      <Navbar fixed="top">
-        <Container fluid>
-          <Navbar.Brand href="#home">Navbar with text</Navbar.Brand>
-          <Navbar.Toggle />
-          <Navbar.Collapse className="justify-content-end">
-            <Nav.Link href="#home">Features</Nav.Link>
-            <Nav.Link href="#link">Team</Nav.Link>
-            <Nav.Link href="#link">Sign In</Nav.Link>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
+        <div  className="container">
+            <div className="row align-items-start">
+            <div className="col">
+                Logo
+            </div>
+            <div className="col">
+                <a href="#home">Features</a>
+            </div>
+            <div className="col">
+                <a href="#link">Team</a>
+            </div>
+            <div className="col">
+                <a href="#">Sign In</a>
+            </div>
+            </div>
+        </div>
+    //   <Navbar style={{backgroundColor: '#181f2a', color: "white"}} fixed="top">
+    //     <Container fluid>
+    //       <Navbar.Brand href="#home"></Navbar.Brand>
+    //       <Navbar.Toggle />
+    //       <Navbar.Collapse className="justify-content-end">
+    //         <Nav.Link href="#home">Features</Nav.Link>
+    //         <Nav.Link href="#link">Team</Nav.Link>
+    //         <Nav.Link href="#link">Sign In</Nav.Link>
+    //       </Navbar.Collapse>
+    //     </Container>
+    //   </Navbar>
     );
   }
   
