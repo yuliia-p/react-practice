a7652d69ad2b5a379cc14cb8667be51f6c475e96
Yuliia Pchelintseva <junoromanova@gmail.com>
2023-03-24 10:11:01 -0700

    working on home component

--- vite-project/src/components/Navbar.jsx
+++ vite-project/src/components/Navbar.jsx
@@ -4,32 +4,18 @@ import logoUrl from "../assets/logo.svg"
 function NavbarComponent() {
     return (
         <div className="container">
-            <div className="row align-items-start">
+            <div className="row align-items-start row-navbar">
             <div className="col">
                 <img src={logoUrl} alt="" />
             </div>
-            <div className="col">
+            <div style={{textAlign: "end"}} className="col">
                 <a href="#home">Features</a>
-            </div>
-            <div className="col">
                 <a href="#link">Team</a>
-            </div>
-            <div className="col">
                 <a href="#">Sign In</a>
-            </div>
+            </div>       
             </div>
         </div>
-    //   <Navbar style={{backgroundColor: '#181f2a', color: "white"}} fixed="top">
-    //     <Container fluid>
-    //       <Navbar.Brand href="#home"></Navbar.Brand>
-    //       <Navbar.Toggle />
-    //       <Navbar.Collapse className="justify-content-end">
-    //         <Nav.Link href="#home">Features</Nav.Link>
-    //         <Nav.Link href="#link">Team</Nav.Link>
-    //         <Nav.Link href="#link">Sign In</Nav.Link>
-    //       </Navbar.Collapse>
-    //     </Container>
-    //   </Navbar>
+    
     );
   }
   
--- vite-project/src/pages/Home.jsx
+++ vite-project/src/pages/Home.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import imgUrl from "../assets/illustration-intro.png"
 import accessImgUrl from "../assets/icon-access-anywhere.svg"
 import securityImgUrl from "../assets/icon-security.svg"
+import collabUrl from "../assets/icon-collaboration.svg"
+import fileUrl from "../assets/icon-any-file.svg"
+import productiveUrl from "../assets/illustration-stay-productive.png"
 
 function Home() {
     return (
@@ -13,7 +16,7 @@ function Home() {
             <p>Access them wherever you need, share and collaborate with friends family, and co-workers.</p>
             <button>Get Started</button>
         </div>
-        <div className="row">
+        <div className="row dark-bg">
             <div style={{textAlign: "center", padding: "3rem"}} className="col">
                 <img src={accessImgUrl} alt="" />
                 <h3>Access your files, anywhere </h3>
@@ -26,8 +29,28 @@ function Home() {
             </div>
         </div>
         <div className="row">
-            <div className="col"></div>
-            <div className="col"></div>
+            <div style={{textAlign: "center", padding: "3rem"}} className="col">
+                <img src={collabUrl} alt="" />
+                <h3>Real-time collaboration</h3>
+                <p>Securely share files and folders with friends, family and colleagues for live collaboration. No email attachments required.</p>
+            </div>
+            <div style={{textAlign: "center", padding: "3rem"}} className="col">
+                <img src={fileUrl} alt="" />
+                <h3>Store any type of file</h3>
+                <p>Whether you're sharing holidays photos or work documents, Fylo has you covered allowing for all file types to be securely stored and shared</p>
+            </div>
+        </div>
+        <div style={{alignItems: "center"}} className="row">
+            <div className="col">
+                <img src={productiveUrl} alt="" />
+            </div>
+            <div className="col">
+                <h1>Stay productive, <br/> wherever you are</h1>
+                <p>Never let location be an issue when accessing your files. Fylo has you covered for all of your file storage needs.</p>
+                <p>Securely share files and folders with friends, family and colleagues for live collaboration. No email attachments required.</p>
+                <a href="#">See how Fylo works</a>
+            </div>
+
         </div>
        </div> 
       )
